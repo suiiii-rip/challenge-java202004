ef85ffb924a8079b6486835e5e47c052385f51bd
Patrick Sy <patrick.sy@get-it.us>
2020-04-27 06:48:01 +0200

    doing logs with streams... fail

--- src/main/java/challenge/logs/Logs.java
+++ src/main/java/challenge/logs/Logs.java
@@ -3,43 +3,41 @@ package challenge.logs;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Logs {
 
-  private static final Pattern PATTERN = Pattern.compile("^[\\d]+$");
+  private static final Pattern PATTERN = Pattern.compile("^[\\d\\s]+$");
 
   public String[] orderLogs(String[] logs) {
 
-    List<String[]> digits = new ArrayList<>();
-    SortedMap<String, String[]> letters = new TreeMap<>();
+    Map<Boolean, List<Tuple<String, String>>> collect = Arrays.stream(logs)
+      .map(s -> s.split(" "))
+      .map(s -> new Tuple<>(s[0], String.join(" ", Arrays.copyOfRange(s, 1, s.length))))
+      .collect(Collectors.partitioningBy(tuple -> isDigit(tuple.b)));
 
-    for (String l : logs) {
+    Stream<Tuple<String, String>> letterLogs = collect.get(false).stream()
+      .sorted(Comparator.comparing((Tuple<String, String> t) -> t.b).thenComparing(t -> t.a));
 
-      String[] split = l.split(" ");
-
-      if (isDigit(split[1])) {
-        digits.add(split);
-      } else {
-        String toOrder = String.join(" ", Arrays.copyOfRange(split, 1, split.length)) + " " + split[0];
-        letters.put(toOrder, split);
-      }
-    }
-
-    List<String[]> sorted = new ArrayList<>(letters.values());
-    sorted.addAll(digits);
-
-    List<String> result = new ArrayList<>();
-
-    for (String[] arr : sorted) {
-      result.add(String.join(" ", arr));
-    }
-
-    return result.toArray(new String[0]);
+    return Stream.concat(letterLogs, collect.get(true).stream())
+      .map(t -> t.a + " " + t.b)
+      .toArray(String[]::new);
   }
 
-  private boolean isDigit(String value) {
-    Matcher matcher = PATTERN.matcher(value);
+  private boolean isDigit(String values) {
+    Matcher matcher = PATTERN.matcher(values);
     return matcher.matches();
+  }
+
+  private static class Tuple<A, B> {
+    private final A a;
+    private final B b;
+
+    Tuple(A a, B b) {
+      this.a = a;
+      this.b = b;
+    }
   }
 
 }
