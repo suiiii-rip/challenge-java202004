b8dc1335ea2c7e5f05ba770ef517ae515e9b45d1
Patrick Sy <patrick.sy@get-it.us>
2020-04-28 05:31:09 +0200

    islands inner class recursion

--- src/main/java/challenge/islands/Islands.java
+++ src/main/java/challenge/islands/Islands.java
@@ -13,43 +13,49 @@ public class Islands {
       searched[i] = new boolean[columns];
     }
 
-    int islands = 0;
+    class SearchIslands {
+      public int search() {
+        int islands = 0;
 
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[i].length; j++) {
+        for (int i = 0; i < rows; i++) {
+          for (int j = 0; j < columns; j++) {
 
-        if (grid[i][j] == 1 && !searched[i][j]) {
-          islands++;
-          searchIsland(i, j, grid, searched);
+            if (grid[i][j] == 1 && !searched[i][j]) {
+              islands++;
+              searchIsland(i, j);
+            }
+
+          }
         }
 
+        return islands;
+      }
+
+      private void searchIsland(int i, int j) {
+        if (searched[i][j]) {
+          return;
+        }
+        searched[i][j] = true;
+
+        if (grid[i][j] == 0) {
+          return;
+        }
+
+        // go right
+        searchIsland(i, Math.min(j + 1, columns - 1));
+
+        // go down
+        searchIsland(Math.min(i + 1, rows - 1), j);
+
+        // go left
+        searchIsland(i, Math.max(j - 1, 0));
+
+        // go up
+        searchIsland(Math.max(i - 1, 0), j);
       }
     }
 
-    return islands;
-  }
-
-  private void searchIsland(int i, int j, int[][]grid, boolean[][] searched) {
-    if (searched[i][j]) {
-      return;
-    }
-    searched[i][j] = true;
-
-    if (grid[i][j] == 0) {
-      return;
-    }
-
-    // go right
-    searchIsland(i, Math.min(j + 1, grid[0].length - 1), grid, searched);
-
-    // go down
-    searchIsland(Math.min(i + 1, grid.length - 1), j, grid, searched);
-
-    // go left
-    searchIsland(i, Math.max(j - 1, 0), grid, searched);
-
-    // go up
-    searchIsland(Math.max(i - 1, 0), j, grid, searched);
+    return new SearchIslands().search();
   }
 
 }
