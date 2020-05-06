06f063884a442e8fe582a9f227e946653a3fb739
Patrick Sy <patrick.sy@get-it.us>
2020-05-06 04:29:01 +0200

    commenting and explaining recursive function

--- src/main/java/challenge/reversekgroup/ReverseNodes.java
+++ src/main/java/challenge/reversekgroup/ReverseNodes.java
@@ -15,17 +15,23 @@ public class ReverseNodes {
   private Node reverse(Node list, int k, int kk, Node reversed) {
     if (kk > 1) {
       if (list.getTail() == NIL) {
+        // list has 1 remaining value while there are more iterations to go left.
+        // Thus the temp. reversed list needs to be put back in order to have a dangling sublist in original order.
         return prependReversed(reversed, list);
       } else {
+        // deconstruct original list and prepend current value to reversed sublist
         return reverse(list.getTail(), k, kk - 1, n(list.getValue(), reversed));
       }
     }
 
     // kk == 0
+    Node currentReversed = n(list.getValue(), reversed);
     if (list.getTail() == NIL) {
-      return n(list.getValue(), reversed);
+      // we found a complete sublist to revert while we reached the end of the original list.
+      return currentReversed;
     }
-    return prepend(n(list.getValue(), reversed), reverse(list.getTail(), k, k, NIL));
+    // prepend the reversed sublist to the remaining list that needs to be reversed.
+    return prepend(currentReversed, reverse(list.getTail(), k, k, NIL));
   }
 
   private Node prependReversed(Node reversed, Node list) {
